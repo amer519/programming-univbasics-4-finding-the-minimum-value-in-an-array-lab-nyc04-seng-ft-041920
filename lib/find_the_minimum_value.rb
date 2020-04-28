798357a6628475a85f2323b46a53d74830d22cb0
Amer Fahmy <afahmy519@gmail.com>
2020-04-28 20:21:04 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,9 +1,9 @@
 def find_min_value(array)
   count = 0
-  min_value = -1 
+  min_value = 10000000000 
   
   while count < array.length do 
-    if min_value > array[count]
+    if min_value < array[count]
       min_value = array[count]
     end
     
